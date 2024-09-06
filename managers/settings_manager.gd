6eec2255e550933cb224829903100a2596c07470
mperez68 <marc.perez.dev@gmail.com>
2024-09-05 23:00:40 -0700

    SPC-21: Settings Menu

--- managers/settings_manager.gd
+++ managers/settings_manager.gd
@@ -1,2 +1,29 @@
 extends Node
-# Does menu settings: Screen size, borderless, audio levels, controls remapping, etc.
+
+enum CHANNELS { MASTER, MUSIC, SFX }
+
+const WINDOW_SIZE = Vector2i(1440, 900)
+
+@onready var FULL_SIZE = DisplayServer.screen_get_size()
+
+
+func full_screen():
+	if (DisplayServer.window_get_mode()) == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		
+		DisplayServer.window_set_size(WINDOW_SIZE)
+	else:
+		DisplayServer.window_set_size(FULL_SIZE)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func borderless():
+	get_tree().root.borderless = !get_tree().root.borderless
+
+func get_audio_level(channel: CHANNELS) -> float:
+	return db_to_linear(AudioServer.get_bus_volume_db(channel))
+
+func set_audio_level(value: float, channel: CHANNELS):
+	AudioServer.set_bus_volume_db(
+		channel,
+		linear_to_db(value)
+	)
--- menu/settings_menu.gd
+++ menu/settings_menu.gd
@@ -1,6 +1,31 @@
 extends Menu
 class_name SettingsMenu
 
+@onready var sliders = [ $AudioSliders/MasterAudio, $AudioSliders/MusicAudio, $AudioSliders/SFXAudio ]
+@onready var settings_manager = $SettingsManager
+
+func _ready():
+	var i = 0
+	for slider in sliders:
+		slider.value = settings_manager.get_audio_level(i)
+		i += 1
 
 func _on_back_pressed() -> void:
 	transition.emit(MENU.CORE)
+
+
+func _on_fullscreen_pressed() -> void:
+	settings_manager.full_screen()
+
+
+func _on_windowed_pressed() -> void:
+	settings_manager.borderless()
+
+
+func _on_audio_value_changed(value: float, channel: int) -> void:
+	settings_manager.set_audio_level(value, channel)
+
+
+func _on_sfx_audio_drag_ended(value_changed: bool) -> void:
+	if (value_changed):
+		$Shoot.play()
